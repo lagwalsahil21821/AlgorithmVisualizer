7d2c8e07fe9af3190d6e487e32194d48d2f8c3cf
lagwalsahil21821 <lagwalsahil7978@gmail.com>
2023-08-24 01:50:23 +0530

    Algorithm Visualizer repo created

---
+++ app/src/main/java/com/example/sortingvisualizerapp/ui/theme/Color.kt
@@ -0,0 +1,16 @@
+package com.example.sortingvisualizerapp.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
+
+object SortingVisualizerColor {
+    val green = Color(0xFF495E57)
+    val yellow = Color(0xFFF4CE14)
+    val pink = Color(0xFFFBDABB)
+    val cloud = Color(0xFFEDEFEE)
+    val charcoal = Color(0xFF333333)
+}
